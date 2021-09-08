ac3392a8a5a7abd519edd803b52e40d5023a31f7
vibhav2000 <vibhav.sharma2000@gmailc=.con>
2021-09-08 07:14:03 +0530

    added student.js and finalised project by testing using postman

--- index.js
+++ index.js
@@ -11,10 +11,9 @@ app.use(express.static('public'));
 app.use(express.json()); //initialised express to parse data sent/received
 
 //making routes
-app.use('/api',require('./routes/api'));
+app.use('/api',require('./router/api'));
 
 app.use(function(err,req,res,next){
-    //console.log(err);
     res.status(422).send({error: err.message});
 });
 
